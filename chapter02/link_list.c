b8e15060bcf3a8be9c5555b3df44297f60b04fc6
小离 <438011578@qq.com>
2023-07-16 23:15:36 +0800

    rename Lnode

--- chapter02/link_list.c
+++ chapter02/link_list.c
@@ -3,11 +3,11 @@
 //
 #include "../status_def.h"
 
-typedef struct Lnode { /* 声明结点的类型和指向结点的指针类型 */
+typedef struct Node { /* 声明结点的类型和指向结点的指针类型 */
     ElemType data; /* 结点的数据域 */
-    struct Lnode *next; /* 结点的指针域 */
-} Lnode, *LinkList; /* LinkList为指向结构体Lnode的指针类型 */
+    struct Node *next; /* 结点的指针域 */
+} Node, *LinkList; /* LinkList为指向结构体Lnode的指针类型 */
 
 int main() {
-    return OK;
+    return 0;
 }
