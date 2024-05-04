434bbc7fb043308d53f8797c157d859773a8529a
Ferris <ferris.evans@outlook.com>
2024-05-04 12:24:55 +0800

    lcp 01

---
+++ leetcode/Lcp01.cpp
@@ -0,0 +1,35 @@
+#include "iostream"
+#include "vector"
+
+using namespace std;
+
+
+class Solution {
+public:
+    int game(vector<int>& guess, vector<int>& answer) {
+
+        int count = 0;
+        for (int i = 0; i < 3; ++i) {
+            if (guess[i] == answer[i]) count++;
+        }
+
+        return count;
+    }
+};
+
+int main () {
+    Solution s;
+    vector<int> guess;
+    vector<int> answer;
+
+    guess.push_back(1);
+    guess.push_back(3);
+    guess.push_back(3);
+
+    answer.push_back(2);
+    answer.push_back(3);
+    answer.push_back(1);
+    int res = s.game(guess, answer);
+    cout << res << endl;
+    return 0;
+}
